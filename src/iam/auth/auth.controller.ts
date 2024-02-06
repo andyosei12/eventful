74cfc12801f3f24a9e931fbea5c3f562be54b33d
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-02-06 11:29:47 +0000

    add guards for protected routes

--- src/iam/auth/auth.controller.ts
+++ src/iam/auth/auth.controller.ts
@@ -2,16 +2,20 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup-dto';
 import { SigninDto } from './dto/signin-dto';
+import { Public } from './decorators/skip-auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
   @Post('signup')
   signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('signin')
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
--- src/iam/interfaces/active-user-data.interface.ts
+++ src/iam/interfaces/active-user-data.interface.ts
@@ -1,7 +1,8 @@
 import { Types } from 'mongoose';
+import { Role } from 'src/users/enums/role.enum';
 
 export interface ActiveUserData {
   sub: Types.ObjectId;
   email: string;
-  role: string;
+  role: Role;
 }
