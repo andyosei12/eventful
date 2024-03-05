0292f715765042ca73fee291a68dc0b9b03cf9f1
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-03-05 06:28:40 +0000

    remove wrong validator import

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate, IsNumber, IsString, isString } from 'class-validator';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 
 export class CreateEventDto {
   @IsString()
