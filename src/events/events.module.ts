349267deee82cbb9129af510401c8fae15758f24
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-03-06 08:01:50 +0000

    add cache layer (#16)

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,10 +3,12 @@ import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { Event, EventSchema } from './schemas/event.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+    CacheModule.register(),
   ],
   controllers: [EventsController],
   providers: [EventsService],
