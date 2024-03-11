a9db82e38fdfb079700ac366a66c2f5e2f8f954f
Andy Osei <andrew.owusu@novanest.net>
2024-03-11 19:13:17 +0000

    add test for auth and user creation

--- src/iam/auth/auth.guard.spec.ts
+++ src/iam/auth/auth.guard.spec.ts
@@ -1,7 +1,16 @@
 import { AuthGuard } from './auth.guard';
-
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 describe('AuthGuard', () => {
+  const reflector = new Reflector();
+  let service: JwtService;
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(
+      new AuthGuard(
+        service,
+        { secret: 'jwtService', accessTokenTtl: 3600 },
+        reflector,
+      ),
+    ).toBeDefined();
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, connect, Model } from 'mongoose';
+import { User, UserSchema } from './schemas/users.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+// import { TestDbModule, closeInMongodConnection } from '../../test/db.module';
 
+jest.setTimeout(100000);
 describe('UsersController', () => {
   let controller: UsersController;
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let userModel: Model<User>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    userModel = mongoConnection.model(User.name, UserSchema);
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getModelToken(User.name), useValue: userModel },
+      ],
     }).compile();
-
-    controller = module.get<UsersController>(UsersController);
+    controller = app.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(async () => {
+    const collections = mongoConnection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  });
+
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  it('should create a user', async () => {
+    const UserDto: CreateUserDto = {
+      email: 'drew@mail.com',
+      first_name: 'Drew',
+      last_name: 'Osei',
+      password: 'willkommen',
+      role: 'regular',
+    };
+    const createdUser = await controller.create(UserDto);
+
+    expect(createdUser.email).toBe(UserDto.email);
   });
 });
